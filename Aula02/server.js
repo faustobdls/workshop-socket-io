8788d5a98ebee2ce4bfe96f508e96c49b4ded346
faustobdls <fausto.level@gmail.com>
2016-10-15 19:05:59 -0300

    Add files via upload

--- Aula02/server.js
+++ Aula02/server.js
@@ -12,22 +12,37 @@ const io = require('socket.io').listen(app)
 
 const users = []
 
+const rooms = ['chat da uol']
+
 io.on('connection', (socket) => {
   console.log('conexão com o Socket.io')
 
   socket.on('disconnect', () => {
     console.log('disconnect no socket')
+    removeUser(socket.user)
+
   })
   socket.on('adduser', (user) => {
     console.log('user', user)
     users.push(user)
-    socket.join('chat da uol')
-    socket.broadcast.emit('enterroom', user)
-    socket.broadcast.emit('listUsers', users);
+    socket.join(rooms[0])
+    socket.room = rooms[0]
+    socket.broadcast.to(rooms[0]).emit('enterroom', user)
+    socket.broadcast.to(rooms[0]).emit('listUsers', users);
     socket.emit('listUsers', users);
     socket.user = user;
   })
-
+  function removeUser(user){
+    let key = users.indexOf(user)
+    if (users[key] === socket.user) {
+      users.splice(key, 1);
+    }
+    socket.broadcast.to(rooms[0]).emit('listUsers', users);
+    socket.emit('listUsers', users);
+  }
+  socket.on('leavechat', (user) => {
+    users.filter(removeUser)
+  })
   socket.on('message', (msg) => {
     console.log('message', msg)
     socket.broadcast.emit('message', msg)
